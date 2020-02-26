760a12c002c6544f3cb3619357412a11d6a75466
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-02-26 23:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,79 @@
 function dwarfRollCall(dwarves) {
+  
+var string = "";
+ for(var i=0 ; i<dwarves.length; i++){
+   
+   string+= `${i+1}. ${dwarves[i]} `;
+    
+   }
+ return string;
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  
+  
+  var array1 =[] ;
+  
+  for(var i=0; i<planeteerCalls.length; i++){
+  
+    array1.push(planeteerCalls[i].toUpperCase() + "!");
 }
+    return array1;
+
+}
+
+
+
 
 function longPlaneteerCalls(words) {
+  for(var i=0; i<words.length; i++){
+    if(words[i].length >4){
+      return  true;
+    }
+  } 
+  return false;
 }
 
+
+
 function findTheCheese (foods) {
+  var cheeseFoods = ["cheddar","gouda","camembert", "swiss"];
+  for(var i=0; i < foods.length; i++){
+    for(var j=0; j< cheeseFoods.length; j++){
+      if(cheeseFoods[j]===foods[i]){
+       return cheeseFoods[j];
+      }  
+    }
+  }
+  return "no cheese!";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
